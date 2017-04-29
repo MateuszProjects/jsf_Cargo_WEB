ddc25cc065623ce45b29c194121e3dba44554318
MateuszProjects <dorobisz.mateusz@o2.pl>
2017-04-29 15:46:29 +0200

    Commit

--- ejbModule/info/dao/AccessDAO.java
+++ ejbModule/info/dao/AccessDAO.java
@@ -7,31 +7,38 @@ import javax.persistence.Query;
 
 import info.entities.Access;
 
-
 @Stateless
 public class AccessDAO {
-	
+
 	private final static String UNIT_NAME = "jsfcourse-CargoPU";
 
 	@PersistenceContext(unitName = UNIT_NAME)
 	protected EntityManager em;
-	
+
 	public Access getLogin(String login, String pass) {
 		Access access = null;
 
 		Query query = em.createQuery("SELECT e From Access e where e.login=:login and e.pass=:pass");
 		query.setParameter("login", login);
 		query.setParameter("pass", pass);
-		
+
 		try {
 			access = (Access) query.getSingleResult();
-			if(access == null){
+			if (access == null) {
 				return null;
+			} else {
+				
+				try {
+					access.getEmployee();
+					access.getCustomer();
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				}
 			}
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
-		
+
 		return access;
 	}
 }
