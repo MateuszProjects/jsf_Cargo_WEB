2e08465b8a7b6ab00d0ea5956800d54a3233b738
MateuszProjects <dorobisz.mateusz@o2.pl>
2017-05-29 19:35:00 +0200

    Commit

--- src/info/Cargo/BB/AddressEditBB.java
+++ src/info/Cargo/BB/AddressEditBB.java
@@ -7,4 +7,35 @@ import javax.faces.bean.ViewScoped;
 @ViewScoped
 public class AddressEditBB {
 
+	private int idaddress;
+
+	private String cityCode;
+
+	private String streat;
+
+	public int getIdaddress() {
+		return idaddress;
+	}
+
+	public void setIdaddress(int idaddress) {
+		this.idaddress = idaddress;
+	}
+
+	public String getCityCode() {
+		return cityCode;
+	}
+
+	public void setCityCode(String cityCode) {
+		this.cityCode = cityCode;
+	}
+
+	public String getStreat() {
+		return streat;
+	}
+
+	public void setStreat(String streat) {
+		this.streat = streat;
+	}
+	
+	
 }
