1f0b8c53c077ebd925f358f204c8ab006b197428
MateuszProjects <dorobisz.mateusz@o2.pl>
2017-05-30 20:30:06 +0200

    Commit

--- src/info/Cargo/BB/AddressBB.java
+++ src/info/Cargo/BB/AddressBB.java
@@ -1,9 +1,14 @@
 package info.Cargo.BB;
 
+import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+
+import info.dao.AddressDAO;
 
 @ManagedBean
 public class AddressBB {
 
+	@EJB
+	AddressDAO addressDAO;
 }
