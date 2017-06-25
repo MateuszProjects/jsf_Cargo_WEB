d44cc98016dd1dd08a175c9adad2c3eb9a70ba97
MateuszProjects <dorobisz.mateusz@o2.pl>
2017-06-25 22:58:56 +0200

    Commit

--- ejbModule/info/dao/CargoDAO.java
+++ ejbModule/info/dao/CargoDAO.java
@@ -1,8 +1,12 @@
 package info.dao;
 
+import java.util.List;
+import java.util.Map;
+
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 import info.entities.Cargo;
 
@@ -25,4 +29,38 @@ public class CargoDAO {
 		em.remove(em.merge(cargo));
 	}
 
+	
+	public List<Cargo> getCargoList(Map<String, Object> searchParams,PaginationInfo info){
+		List<Cargo> list = null;
+		
+		String select = "select c ";
+		String from = "from Cargo c ";
+		String where = "";
+		String join = "";
+		
+		
+		Query querycount = em.createQuery("SELECT COUNT(c.idcargo) " + from + join + where);
+		
+		
+		try {
+			Number n = (Number) querycount.getSingleResult();
+			info.setCount(n.intValue());
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		Query query = em.createQuery(select + from + join + where);
+		query.setFirstResult(info.getOffset());
+		query.setMaxResults(info.getLimit());
+		
+		try {
+			list = query.getResultList();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+		return list;
+	}
 }
