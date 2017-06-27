7f88b9bc31bdd9c2e36d11fb05c131f0a0c38aad
MateuszProjects <dorobisz.mateusz@o2.pl>
2017-06-27 21:37:20 +0200

    Commit

--- ejbModule/info/dao/DeliveryhistoryDAO.java
+++ ejbModule/info/dao/DeliveryhistoryDAO.java
@@ -1,10 +1,15 @@
 package info.dao;
 
+import java.util.List;
+import java.util.Map;
+
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 import info.entities.Deliveryhistory;
+
 
 @Stateless
 public class DeliveryhistoryDAO {
@@ -24,5 +29,36 @@ public class DeliveryhistoryDAO {
 
 	public void remove(Deliveryhistory deliveryhistory) {
 		em.remove(em.merge(deliveryhistory));
+	}
+	
+	public List<Deliveryhistory>  getSearchList(Map<String, Object> searchParams,PaginationInfo info){
+		List<Deliveryhistory> list = null;
+		String select = "select c ";
+		String from = "from Deliveryhistory c ";
+		String where = "";
+		String orderby = "";
+		String join = "";
+
+		Query querycount = em.createQuery("SELECT COUNT(c.iddeliveryHistory) " + from + join + where);
+
+		try {
+			Number n = (Number) querycount.getSingleResult();
+			info.setCount(n.intValue());
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		Query query = em.createQuery(select + from + join + where);
+		query.setFirstResult(info.getOffset());
+		query.setMaxResults(info.getLimit());
+
+		try {
+			list = query.getResultList();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return list;
 	}
 }
