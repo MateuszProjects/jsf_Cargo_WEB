c301e4164bae2939b4e765a11b1e75c94a832c00
MateuszProjects <dorobisz.mateusz@o2.pl>
2017-06-28 21:17:40 +0200

    Commit

--- ejbModule/info/dao/LoactionDAO.java
+++ ejbModule/info/dao/LoactionDAO.java
@@ -1,8 +1,12 @@
 package info.dao;
 
+import java.util.List;
+import java.util.Map;
+
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 import info.entities.Location;
 
@@ -24,5 +28,38 @@ public class LoactionDAO {
 
 	public void remove(Location location) {
 		em.remove(em.merge(location));
+	}
+	
+	public List<Location> getSearchList(Map<String, Object> searchParams,PaginationInfo info){
+		List<Location> list = null;
+		
+
+		String select = "select c ";
+		String from = "from Location c ";
+		String where = "";
+		String orderby = "";
+		String join = "";
+
+		Query querycount = em.createQuery("SELECT COUNT(c.idlocation) " + from + join + where);
+
+		try {
+			Number n = (Number) querycount.getSingleResult();
+			info.setCount(n.intValue());
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		Query query = em.createQuery(select + from + join + where);
+		query.setFirstResult(info.getOffset());
+		query.setMaxResults(info.getLimit());
+
+		try {
+			list = query.getResultList();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return list;
 	}
 }
