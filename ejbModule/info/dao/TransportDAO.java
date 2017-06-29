c40e650c53e9fd540983f31db2d8bf700e5ed123
MateuszProjects <dorobisz.mateusz@o2.pl>
2017-06-29 21:14:48 +0200

    Commit

--- ejbModule/info/dao/TransportDAO.java
+++ ejbModule/info/dao/TransportDAO.java
@@ -1,28 +1,66 @@
 package info.dao;
 
+import java.util.List;
+import java.util.Map;
+
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 import info.entities.Transport;
 
 @Stateless
 public class TransportDAO {
-	
+
 	private final static String UNIT_NAME = "jsfcourse-CargoPU";
 
 	@PersistenceContext(unitName = UNIT_NAME)
 	protected EntityManager em;
-	
+
 	public Transport find(Integer id) {
 		return em.find(Transport.class, id);
 	}
 
-	public void createTransport(Transport transport ) {
+	public void createTransport(Transport transport) {
 		em.persist(transport);
 	}
 
 	public void remove(Transport transport) {
 		em.remove(em.merge(transport));
+	}
+
+	public List<Transport> getSearchList(Map<String, Object> searchParams, PaginationInfo info) {
+
+		List<Transport> list = null;
+
+		String select = "select c ";
+		String from = "from Transport c ";
+		String where = "";
+		String orderby = "";
+		String join = "";
+
+		Query querycount = em.createQuery("SELECT COUNT(c.idtransport) " + from + join + where);
+
+		try {
+			Number n = (Number) querycount.getSingleResult();
+			info.setCount(n.intValue());
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		Query query = em.createQuery(select + from + join + where);
+		query.setFirstResult(info.getOffset());
+		query.setMaxResults(info.getLimit());
+
+		try {
+			list = query.getResultList();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return list;
+
 	}
 }
