63a58c9cdceaec3ae6874c3ffad4cd3a2451ebaf
MateuszProjects <dorobisz.mateusz@o2.pl>
2017-06-29 21:14:35 +0200

    Commit

--- src/info/Cargo/BB/LazyDataModelTransport.java
+++ src/info/Cargo/BB/LazyDataModelTransport.java
@@ -1,14 +1,60 @@
 package info.Cargo.BB;
 
-import org.primefaces.model.LazyDataModel;
+import java.util.List;
+import java.util.Map;
 
+import org.primefaces.model.LazyDataModel;
+import org.primefaces.model.SortOrder;
+
+import info.dao.PaginationInfo;
+import info.dao.TransportDAO;
+import info.entities.Location;
 import info.entities.Transport;
 
-public class LazyDataModelTransport extends LazyDataModel<Transport>{
+public class LazyDataModelTransport extends LazyDataModel<Transport> {
 
 	/**
 	 * 
 	 */
-	private static final long serialVersionUID = -2332146630086957673L;
+	private static final long serialVersionUID = 1L;
+
+	TransportDAO transportDAO;
+
+	private Map<String, Object> searchParams;
+
+	public Map<String, Object> getSearchParams() {
+		return searchParams;
+	}
+
+	public void setSearchParams(Map<String, Object> searchParams) {
+		this.searchParams = searchParams;
+	}
+
+	public void setTransportDAO(TransportDAO transportDAO) {
+		this.transportDAO = transportDAO;
+	}
+
+	@Override
+	public Transport getRowData(String rowKey) {
+		return null;
+	}
+
+	@Override
+	public Object getRowKey(Transport transport) {
+		return null;
+	}
+
+	@Override
+	public List<Transport> load(int first, int pageSize, String sortField, SortOrder sortOrder,
+			Map<String, Object> filters) {
+		List<Transport> list = null;
+		PaginationInfo info = new PaginationInfo();
+		info.setLimit(pageSize);
+		info.setOffset(first);
+
+		setRowCount(info.getCount());
+		list = transportDAO.getSearchList(searchParams, info);
+		return list;
+	}
 
 }
--- src/info/Cargo/BB/TransportBB.java
+++ src/info/Cargo/BB/TransportBB.java
@@ -1,8 +1,11 @@
 package info.Cargo.BB;
 
+import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
@@ -10,12 +13,53 @@ import javax.faces.context.FacesContext;
 
 import org.primefaces.context.RequestContext;
 import org.primefaces.event.SelectEvent;
+import org.primefaces.model.LazyDataModel;
+
+import info.dao.TransportDAO;
+import info.entities.Transport;
 
 @ManagedBean
 @ViewScoped
-public class TransportBB {
+public class TransportBB implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
 	public void showMessage() {
-		RequestContext.getCurrentInstance().showMessageInDialog(new FacesMessage(FacesMessage.SEVERITY_INFO, "What we do in life", "Echoes in eternity."));
-		}
+		RequestContext.getCurrentInstance().showMessageInDialog(
+				new FacesMessage(FacesMessage.SEVERITY_INFO, "What we do in life", "Echoes in eternity."));
+	}
+
+	@EJB
+	TransportDAO transportDAO;
+
+	private LazyDataModelTransport lazyModel;
+
+	public LazyDataModelTransport getLazyModel() {
+		return lazyModel;
+	}
+
+	public void setLazyModel(LazyDataModelTransport lazyModel) {
+		this.lazyModel = lazyModel;
+	}
+
+	public void setTransportDAO(TransportDAO transportDAO) {
+		this.transportDAO = transportDAO;
+	}
+
+	@PostConstruct
+	public void init() {
+		lazyModel = new LazyDataModelTransport();
+	}
+
+	public LazyDataModel<Transport> getLazyList() {
+		Map<String, Object> searchParams = new HashMap<String, Object>();
+		lazyModel.setSearchParams(searchParams);
+		lazyModel.setTransportDAO(transportDAO);
+		return lazyModel;
+
+	}
+
 }
