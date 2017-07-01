76310055e66a5e94a19ba81cf4afc2f31846c4f8
MateuszProjects <dorobisz.mateusz@o2.pl>
2017-07-01 15:46:05 +0200

    Commit

--- src/info/Cargo/BB/AddressBB.java
+++ src/info/Cargo/BB/AddressBB.java
@@ -7,6 +7,7 @@ import java.util.Map;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
@@ -17,6 +18,7 @@ import org.primefaces.model.LazyDataModel;
 
 import info.dao.AddressDAO;
 import info.entities.Address;
+import info.entities.User;
 import info.lazydatamodel.LazyDataModelAddress;
 
 @ManagedBean
@@ -27,6 +29,64 @@ public class AddressBB implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+
+	private Integer idAddress;
+	private Integer idUser;
+	private String cityCode;
+	private String street;
+	private String telephone;
+	private String email;
+
+	User user = new User();
+	Address address = new Address();
+
+	public Integer getIdAddress() {
+		return idAddress;
+	}
+
+	public void setIdAddress(Integer idAddress) {
+		this.idAddress = idAddress;
+	}
+
+	public Integer getIdUser() {
+		return idUser;
+	}
+
+	public void setIdUser(Integer idUser) {
+		this.idUser = idUser;
+	}
+
+	public String getCityCode() {
+		return cityCode;
+	}
+
+	public void setCityCode(String cityCode) {
+		this.cityCode = cityCode;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
 	@EJB
 	AddressDAO addressDAO;
@@ -63,6 +123,60 @@ public class AddressBB implements Serializable {
 			return "confirm";
 		} else {
 			return event.getNewStep();
+		}
+
+	}
+
+	private boolean validate() {
+		FacesContext ctx = FacesContext.getCurrentInstance();
+		boolean result = false;
+
+		if (idAddress == null) {
+			ctx.addMessage(null, new FacesMessage("idAddress Wymagane"));
+		}
+
+		if (cityCode == null) {
+			ctx.addMessage(null, new FacesMessage("cityCode Wymagane"));
+		}
+
+		if (street == null) {
+			ctx.addMessage(null, new FacesMessage("street Wymagane"));
+		}
+
+		if (telephone == null) {
+			ctx.addMessage(null, new FacesMessage("telephone Wymagane"));
+		}
+
+		if (email == null) {
+			ctx.addMessage(null, new FacesMessage("email Wymagane"));
+		}
+
+		if (ctx.getMessageList().isEmpty()) {
+			address.setIdaddress(idAddress);
+			address.setCityCode(cityCode);
+			address.setTelephone(telephone);
+			address.setStreat(street);
+			address.setEmail(email);
+			address.setUser(user);
+			result = true;
+		}
+		return result;
+	}
+
+	public void save() {
+
+		if (address == null) {
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Brak objektu address"));
+		}
+
+		if (!validate()) {
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Validate wrong"));
+		}
+
+		try {
+			addressDAO.createAddress(address);
+		} catch (Exception ex) {
+			ex.printStackTrace();
 		}
 
 	}
--- src/info/Cargo/BB/EmployeeBB.java
+++ src/info/Cargo/BB/EmployeeBB.java
@@ -6,39 +6,157 @@ import java.util.Map;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 
+import org.primefaces.context.RequestContext;
+import org.primefaces.event.FlowEvent;
 import org.primefaces.model.LazyDataModel;
 
 import info.dao.UserDAO;
 import info.entities.User;
 import info.lazydatamodel.LazyDataModelEmployee;
 
-
 @ManagedBean
 @ViewScoped
-public class EmployeeBB  implements Serializable{
+public class EmployeeBB implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
+	// private Integer Iduser;
+	private String Name;
+	private String Surname;
+	private String Login;
+	private String pass;
+
+	
+
+
+	public String getName() {
+		return Name;
+	}
+
+	public void setName(String name) {
+		Name = name;
+	}
+
+	public String getSurname() {
+		return Surname;
+	}
+
+	public void setSurname(String surname) {
+		Surname = surname;
+	}
+
+	public String getLogin() {
+		return Login;
+	}
+
+	public void setLogin(String login) {
+		Login = login;
+	}
+
+	public String getPass() {
+		return pass;
+	}
+
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+
 	@EJB
 	UserDAO userDAO;
-	
+
+	User user = new User();
+
 	private LazyDataModelEmployee lazyModel;
-	
-	
+
 	@PostConstruct
 	public void init() {
 		lazyModel = new LazyDataModelEmployee();
 	}
-	
-	public LazyDataModel<User>  getLazylist() {
+
+	public LazyDataModel<User> getLazylist() {
 		Map<String, Object> searchParams = new HashMap<String, Object>();
 
 		lazyModel.setSearchParams(searchParams);
 		lazyModel.setUserDAO(userDAO);
 		return lazyModel;
 	}
+
+	private boolean skip;
+
+	public boolean isSkip() {
+		return skip;
+	}
+
+	public void setSkip(boolean skip) {
+		this.skip = skip;
+	}
+
+	public String onFlowProcess(FlowEvent event) {
+		if (skip) {
+			skip = false; // reset in case user goes back
+			return "confirm";
+		} else {
+			return event.getNewStep();
+		}
+
+	}
+
+	private boolean validate() {
+		FacesContext ctx = FacesContext.getCurrentInstance();
+		boolean result = false;
+		
+
+		
+		if (Name == null) {
+			ctx.addMessage(null, new FacesMessage("Name Wymagane"));
+		}
+		
+		if (Surname == null) {
+			ctx.addMessage(null, new FacesMessage("Surname Wymagane"));
+		}
+		
+		if (Login == null) {
+			ctx.addMessage(null, new FacesMessage("idAddress Wymagane"));
+		}
+		
+		if (pass == null) {
+			ctx.addMessage(null, new FacesMessage("pass Wymagane"));
+		}
+		
+		if (ctx.getMessageList().isEmpty()) {
+			
 	
+			user.setName(Name);
+			user.setSurname(Surname);
+			user.setLogin(Login);
+			user.setPass(pass);
+			result = true;
+		}
+		
+		return result;
+	}
+
+	public void save() {
+		if (user == null) {
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Brak objektu user"));
+		}
+
+		if (!validate()) {
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Validate wrong"));
+		}
+		
+		try{
+			userDAO.createUser(user);
+		}catch(Exception ex){
+			ex.printStackTrace();
+		}
+		RequestContext.getCurrentInstance().reset("panelAdd");
+
+	}
+
 }
