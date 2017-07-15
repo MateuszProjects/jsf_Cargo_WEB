eb8cc26051c9e88316071f9b1fa67768dd9d55c1
MateuszProjects <dorobisz.mateusz@o2.pl>
2017-07-15 22:17:28 +0200

    Commit

--- ejbModule/info/dao/LoactionDAO.java
+++ ejbModule/info/dao/LoactionDAO.java
@@ -12,12 +12,12 @@ import info.entities.Location;
 
 @Stateless
 public class LoactionDAO {
-	
+
 	private final static String UNIT_NAME = "jsfcourse-CargoPU";
 
 	@PersistenceContext(unitName = UNIT_NAME)
 	protected EntityManager em;
-	
+
 	public Location find(Integer id) {
 		return em.find(Location.class, id);
 	}
@@ -29,14 +29,13 @@ public class LoactionDAO {
 	public void remove(Location location) {
 		em.remove(em.merge(location));
 	}
-	
-	public Location merge(Location location){
+
+	public Location merge(Location location) {
 		return em.merge(location);
 	}
-	
-	public List<Location> getSearchList(Map<String, Object> searchParams,PaginationInfo info){
+
+	public List<Location> getSearchList(Map<String, Object> searchParams, PaginationInfo info) {
 		List<Location> list = null;
-		
 
 		String select = "select c ";
 		String from = "from Location c ";
@@ -44,19 +43,27 @@ public class LoactionDAO {
 		String orderby = "";
 		String join = "";
 
-		
-		/*if (idAddress != null) {
-		if (where.isEmpty()) {
-			where = "where ";
-		} else {
-			where += " or ";
+		Integer idLocation = (Integer) searchParams.get("idlocation");
+		String description = (String) searchParams.get("description");
+
+		if (idLocation != null) {
+			if (where.isEmpty()) {
+				where = "where ";
+			} else {
+				where += " or ";
+			}
+			where += " c.idlocation like :idlocation ";
 		}
-		if (join.isEmpty()) {
-			join = " join p.idaddress p  ";
-		}
-		where += " c.idCustomer like :idCustomer ";
-	}*/
 		
+		if (description != null) {
+			if (where.isEmpty()) {
+				where = "where ";
+			} else {
+				where += " or ";
+			}
+			where += " c.description like :description ";
+		}
+
 		Query querycount = em.createQuery("SELECT COUNT(c.idlocation) " + from + join + where);
 
 		try {
@@ -76,7 +83,7 @@ public class LoactionDAO {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		return list;
 	}
 }
