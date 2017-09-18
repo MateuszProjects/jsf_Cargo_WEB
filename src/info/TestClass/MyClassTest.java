c287c79f86cd71461ce8f0487984359c8dc7639f
MateuszProjects <dorobisz.mateusz@o2.pl>
2017-09-18 15:39:47 +0200

    Commit

---
+++ src/info/TestClass/MyClassTest.java
@@ -0,0 +1,14 @@
+package info.TestClass;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MyClassTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
