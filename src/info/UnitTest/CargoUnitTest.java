e48845256b5fcabb4d4341d7f2a8928120aa3d36
MateuszProjects <dorobisz.mateusz@o2.pl>
2017-09-21 17:22:04 +0200

    Commit

--- src/info/UnitTest/CargoUnitTest.java
+++ src/info/UnitTest/CargoUnitTest.java
@@ -4,11 +4,27 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import info.Cargo.BB.AddressBB;
+import junit.framework.Assert;
+
 public class CargoUnitTest {
 
+	AddressBB addressBB = new AddressBB();
+	
+	
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void validateAddress() {
+	
+		// Given
+		addressBB.setCityCode("45222");
+		addressBB.save();
+		
+		// When
+		boolean result = true; 
+		
+		// Then
+		assertEquals(true, result);
 	}
 
 }
